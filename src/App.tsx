b0d438b8d56a0c5230a9f52c444df9c191df24f9
Joshua Allan <joshua.allan@ba.com>
2024-09-06 16:10:32 +0100

    General: updated icon colour

--- src/App.tsx
+++ src/App.tsx
@@ -37,9 +37,9 @@ function App() {
             <a
               href="#"
               data-target="slide-out"
-              className="sidenav-trigger right"
+              className="sidenav-trigger right black-text"
             >
-              <i className="material-icons">menu</i>
+              <i className="material-icons ">menu</i>
             </a>
           </div>
         </nav>
